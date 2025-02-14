75626e9dd8b81371d370728d7074648e1da0a212
Mathias Greve Johannessen <mathigj@ulrik.uio.no>
2025-02-14 19:17:23 +0100

    create Profile-User model

--- booking/models.py
+++ booking/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-# class User()
+class Profile(models.Model):
+    ROLE_CHOICES = (
+        ('customer', 'Customer'),
+        ('trainer', 'Trainer'),
+        ('admin', 'Admin')
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.role})"
