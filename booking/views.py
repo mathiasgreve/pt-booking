8dd895bd6344ee621fd35ebcc6878dbdfe592cb4
Mathias Greve Johannessen <mathigj@ulrik.uio.no>
2025-02-14 19:18:39 +0100

    update template and views for booking page - showing header

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from appointment.models import Service
+
+navbarContent = [
+    {"url_name": "pt_presentation:hjem", "base_name": "hjem", "display_name": "Hjem"},
+    {"url_name": "pt_presentation:om_meg", "base_name": "om_meg", "display_name": "Om meg"},
+    {"url_name": "pt_presentation:blog", "base_name": "blog", "display_name": "Blog"},
+    {"url_name": "pt_presentation:treningsfilosofi", "base_name": "treningsfilosofi", "display_name": "Treningsfilosofi"},
+    {"url_name": "pt_presentation:ernaering", "base_name": "ernaering", "display_name": "Ernæring"},
+]
 
 # Create your views here.
 def booking(request):
-    return render(request, "booking/booking.html")
+    pt_services = Service.objects.all()
+    return render(request, "booking/booking.html", {
+        "pt_services": pt_services,
+        "navbarContent": navbarContent
+    })
